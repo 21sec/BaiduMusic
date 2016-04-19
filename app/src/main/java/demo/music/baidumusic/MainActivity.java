6e1227c99eb8fe475c791542dd39c76adf75c5cd
21sec <absence@outlook.com>
2016-04-19 20:25:15 +0800

    add Toolbar and icon

--- app/src/main/java/demo/music/baidumusic/MainActivity.java
+++ app/src/main/java/demo/music/baidumusic/MainActivity.java
@@ -2,6 +2,8 @@ package demo.music.baidumusic;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,20 +13,47 @@ import com.android.volley.toolbox.Volley;
 public class MainActivity extends AppCompatActivity {
     final static String TAG = "BaiduMusic";
     RequestQueue mQueue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Title
+        toolbar.setTitle("BaiduMusic");
+        // Sub Title
+        toolbar.setSubtitle("0.1Base");
+        setSupportActionBar(toolbar);
+        //设置 navigation 需要在setSupportActionBar之后
+        toolbar.setNavigationIcon(R.mipmap.ab_android);
+        //toolbar 右侧的图标点击后，触发回调
+        toolbar.setOnMenuItemClickListener(onMenuItemClick);
+
+        //生成volley的工作队列
         mQueue = Volley.newRequestQueue(getApplicationContext());
     }
 
 
+    private Toolbar.OnMenuItemClickListener onMenuItemClick = new Toolbar.OnMenuItemClickListener() {
+        @Override
+        public boolean onMenuItemClick(MenuItem menuItem) {
+            String msg = "";
+            switch (menuItem.getItemId()) {
+                case R.id.action_edit:
+                    msg += "Click edit";
+                    break;
+                case R.id.action_search:
+                    msg += "Click search";
+                    break;
+            }
+            if (!msg.equals("")) {
+                Log.e(TAG,"Menu Item Click test : " + msg);
+            }
+            return true;
+        }
 
+    };
 
-
-
-
-    /* 以下是studio自动生成的,暂时没什么鸟用 */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -32,18 +61,4 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
