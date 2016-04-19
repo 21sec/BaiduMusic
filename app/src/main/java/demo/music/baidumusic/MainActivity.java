81cb2781e5e5f5a92cfefbefe7b5610d01d3cd41
21sec <790831337@qq.com>
2016-04-19 14:00:49 +0800

    Android studio git test

--- app/src/main/java/demo/music/baidumusic/MainActivity.java
+++ app/src/main/java/demo/music/baidumusic/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-    final static String TAG = "baidumusic";
+    final static String TAG = "BaiduMusic";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
